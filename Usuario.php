0f326b43c18cd1ee789e8a8f16acdfe49a73e7a3
programador-santa <jairoporrasapple@gmail.com>
2025-04-07 13:20:22 -0500

    comentarios

--- Usuario.php
+++ Usuario.php
@@ -8,7 +8,7 @@ class Usuario {
     public function __construct() {
         $this->conexion = (new Database())->getConnection();
     }
-
+// funcion para ver la lista de usuarios 
     public function listarUsuarios() {
         $sql = "SELECT * FROM personas";
         $stmt = $this->conexion->prepare($sql);
@@ -16,7 +16,7 @@ class Usuario {
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-
+//funcion para obtener los datos de un usuario en especifico con su id
     public function obtenerUsuario($id) {
         $sql = "SELECT * FROM personas WHERE id = :id";
         $stmt = $this->conexion->prepare($sql);
@@ -25,7 +25,7 @@ class Usuario {
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-
+//funcion para crear un usuario nuevo con todos sus datos y quede guardado en la base de datos
     public function crearUsuario($primernombre, $segundonombre, $primerapellido, $segundoapellido, $edad, $telefono, $correo, $direccion) {
         $sql = "INSERT INTO personas (primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,edad,telefono,correo,direccion)
         VALUES ('Carlos','Hernan','Molina','Arenas','20','3102396198',carloshernan@gmail.com,calle26#54-64)";
@@ -42,7 +42,7 @@ class Usuario {
 
         return $stmt->execute();
     }
-
+//funcion para actualizar datos del usuario
     public function actualizarUsuario() {
         $sql = "UPDATE personas 
         SET primer_nombre = :primernombre, segundo_nombre = :segundo_nombre, primer_apellido = :primerapellido, segundo_apellido = :segundoapellido, edad = :edad, telefono = :telefono, correo = :corre, direccion = :direccion
@@ -55,8 +55,10 @@ class Usuario {
         $stmt->bindParam(':segundoapellido', $segundoapellido);
         $stmt->bindParam(':edad', $edad);
         $stmt->bindParam(':telefono', $telefono);
+        $stmt->bindParam(':correo', $correo); 
+        $stmt->bindParam(':direccion', $direccion);
     }
-
+//funcion para eliminar usuario
     public function eliminarUsuario($id)
     {
         $sql = "DELETE FROM  personas WHERE id = :id";
